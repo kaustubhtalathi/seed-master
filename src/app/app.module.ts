8ec8220e14937b4dec0fb0a4f5ccf289b0c13342
Kaustubh <kaustubhtalathi@gmail.com>
2017-10-10 23:32:00 -0400

    static content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+
+import {MatButtonModule, MatCheckboxModule,MatGridListModule} from '@angular/material';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule,MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
